91d6f065e02c9476d79f7c122c9b75cd40a1d671
Malcolm Studd <mstudd@recognia.com>
2015-12-11 11:40:18 -0500

    0.0.3

--- lib/Yum/Line.pm
+++ lib/Yum/Line.pm
@@ -13,7 +13,7 @@ use namespace::clean;
 # Keep these in namespace
 use MooX::Options  protect_argv => 0;
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 option config_file => (
 	is      => 'ro',
