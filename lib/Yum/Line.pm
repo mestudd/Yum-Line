23b0bfa2a05ce61a1295102f815e2aeaffc275ba
Malcolm Studd <mstudd@recognia.com>
2016-02-09 16:42:44 -0500

    Add configuration documentation.

--- lib/Yum/Line.pm
+++ lib/Yum/Line.pm
@@ -152,7 +152,80 @@ to use different releases of CentOS. Line third-party syncronises EPEL and
 PostgreSQL 9.2 from their upstream sources. Line local controls locally-built
 packages.
 
-See scripts/yum-line for helpful operations. Typical usage should be init, rsync, load dev, promote dev.
+See scripts/yum-line for helpful operations. Typical usage should be init,
+rsync, load dev, promote dev.
+
+=head1 CONFIGURATION
+
+Configuration is stored in a JSON-formatted file, passed to the script via
+the C<--config> option. There are four parts to the configuration
+
+=head2 directory
+
+The C<directory> configuration value is the absolute path to the filesystem
+location for the repository structure. All the individual repositories are
+placed under this directory.
+
+=head2 base
+
+The C<base> configuration value is an object describing the upstream OS
+repositories and the line configuration. It accepts the configuration for an
+L</upstream> object.
+
+The C<name> is the OS train name. See L</trains> for more information.
+
+The C<sync> value defines the upstream location template for the OS
+repositories. It may contain three special values, which are replaced for the
+specific repository being pulled: C<$rel> will be replaced with the C<version>,
+C<$repo> with the repository name, and C<$arch> with the architecture (currently
+hard-coded to x86_64).
+
+FIXME C<obsolete>/C<vault>/C<archive>
+
+The C<upstream> configuration is an array of OS repositories to use. Do not
+include "os" as it is specially handled.
+
+The C<version> value is the OS major release. Eg. 5, 6, 7.
+
+C<stops> defines the stops on the repository lines. Packages are loaded from
+upstream repositories to the first stop, and then promoted up the line to the
+final stop.
+
+Each stop must have a C<name> to identify it. A repository is created for each
+train-stop pair, located in a directory named just that. Each stop must also
+have a C<version>, specifying the OS version used for that particular stop. The
+os and base repositories sync that version. This is useful for ensuring that
+production repo doesn't get updated without going through the train because of
+an OS release.
+
+The stop may also have an C<obsolete> value. This indicates that the version is
+obsolete and no longer available from upstream. The repositories will be
+mirrored from the archive location instead.
+
+=head2 upstream
+
+The C<upstream> configuration value is an array of objects describing the
+source repositories from which the packages are taken. Each upstream repository
+must have a unique C<name> to identify it. The C<name> is also used as the
+directory name containing the repository on disk.
+
+An upstream repository may have a C<sync> confguration value, which defines the
+location from which the local repository is mirrored. An upstream repository
+without a C<sync> value is not mirrored. This is useful for repositories of
+locally-built packages.
+
+An upstream repository may have a C<restrict> configuration value. If
+specified, it restricts the packages pulled to those matching. The value is
+an array of package names. This is useful for using only a few packages from a
+large upstream like EPEL.
+
+=head2 trains
+
+The C<trains> configuration value is an array of objects describing the sets of
+repositories. The C<name> value must be specified. There is a repository for
+each stop in a directory named for the train and stop names. The C<upstream>
+value must be specified as an array of L</upstream> names defining the package
+sources for the train.
 
 =head1 AUTHOR
 
